513be05fe2a454ecfce06735403fb4238846d33d
lorrainehanlon <lorrainehanlon@hotmail.co.uk>
2021-08-05 20:54:24 +0000

    Add modal snippet to index.html and code to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,44 @@ function initMap(activity) {
       }
     }
   }
+
+  function handleSubmit(event) {
+    // Prevent the default submit action 
+    event.preventDefault();
+
+    // Get input elements
+    let firstname = document.getElementById('firstname');
+    
+
+    // Log values to the console
+    console.log('firstname', firstname.value);
+   
+}
+document.getElementById("myBtn").addEventListener("click", handleSubmit);
+
+
+// Get the modal
+var modal = document.getElementById("submit");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
